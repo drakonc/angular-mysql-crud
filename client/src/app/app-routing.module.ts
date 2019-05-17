08e2a37aff8aebc8736ec052dc2acd89ea0a8eb0
Jose Castro <drakonc@users.noreply.github.com>
2019-05-17 18:30:48 -0500

    Alistado la Navegacion

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { GameListComponent } from "./components/game-list/game-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/games",
+    pathMatch: "full"
+  },
+  {
+    path: "games",
+    component: GameListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
