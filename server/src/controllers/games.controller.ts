01a14b72835a0468a8a3d43ee1f8814ccc5e04da
Jose Castro <drakonc@users.noreply.github.com>
2019-05-17 15:20:27 -0500

    Api Terminada

--- server/src/controllers/games.controller.ts
+++ server/src/controllers/games.controller.ts
@@ -2,33 +2,54 @@ import { Request, Response } from "express";
 import pool from "../database";
 
 class GamesController {
-  public list(req: Request, res: Response) {
-    res.json({ text: "Buscando Todos Juego" });
+  public async list(req: Request, res: Response) {
+    const result = await pool.query("SELECT * FROM games");
+    res.json({ result });
   }
 
-  public getOne(req: Request, res: Response) {
-    res.json({ text: "Buscando un Juego " + req.params.id });
+  public async getOne(req: Request, res: Response) {
+    const { id } = req.params;
+    const result = await pool.query("SELECT * FROM games WHERE id = ?", [id]);
+    if (result.length > 0) {
+      return res.json(result[0]);
+    }
+    res.status(404).json({ result: "Error al Buscar el Juego " + result.title });
   }
 
   public async create(req: Request, res: Response) {
     const { title, description, image } = req.body;
-    const result = await pool.query("INSERT INTO games SET ?", [
-      { title, description, image }
-    ]);
-    console.log(result.affectedRows);
-    if (result.affectedRows != 0) {
-      res.json({ title, description, image });
+    const result = await pool.query("INSERT INTO games SET ?", [{ title, description, image }]);
+    if (result.serverStatus === 2) {
+      res.json({ result: "Se Inserto el Juego " + title });
     } else {
-      res.json({ text: "error" });
+      res.json({ result: "Error al Insertar el Juego " + title });
     }
   }
 
-  public updated(req: Request, res: Response) {
-    res.json({ text: "Actualizando un juego " + req.params.id });
+  public async updated(req: Request, res: Response) {
+    const { id } = req.params;
+    const { title, description, image } = req.body;
+    const result = await pool.query("UPDATE games set ? WHERE id = ?", [{ title, description, image }, id]);
+    if (result.serverStatus === 2) {
+      res.json({ result: "Se Actualizo el Juego " + title + " Correctamente" });
+    } else {
+      res.json({ result: "Error al Actualizanr el Juego " + title });
+    }
   }
 
-  public delete(req: Request, res: Response) {
-    res.json({ text: "Eliminando un juego " + req.params.id });
+  public async delete(req: Request, res: Response) {
+    const { id } = req.params;
+    const resul = await pool.query("SELECT * FROM games WHERE id = ?", [id]);
+    if (resul.length > 0) {
+      const sql = await pool.query("DELETE FROM games WHERE id = ?", [id]);
+      if (sql.serverStatus === 2) {
+        res.json({ result: "Se Elimino el Juego Correctamente" });
+      } else {
+        res.json({ result: "Error al Eliminar el Juego" });
+      }
+    } else {
+      res.json({ result: "El Juego seleccionado no se Encontro " });
+    }
   }
 }
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,40 +1,38 @@
-import express, { Application } from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
+import express, { Application } from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 //Importar Rutas
-import indexRoutes from  './routers/indexRoutes';
-import gameRoutes from './routers/gameRoutes';
+import indexRoutes from "./routes/index.routes";
+import gameRoutes from "./routes/game.routes";
 
 class Server {
+  public app: Application;
 
-    public app: Application;
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+  }
 
-    constructor(){
-        this.app = express();
-        this.config();
-        this.routes();
-    }
+  config(): void {
+    this.app.set("port", process.env.PORT || 3000);
+    this.app.use(morgan("dev"));
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+  }
 
-    config(): void{
-        this.app.set('port', process.env.PORT || 3000);
-        this.app.use(morgan('dev'));
-        this.app.use(cors());
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({extended: false}));
-    }
+  routes(): void {
+    this.app.use("/", indexRoutes);
+    this.app.use("/api/games", gameRoutes);
+  }
 
-    routes(): void {
-        this.app.use('/',indexRoutes);
-        this.app.use('/api/games',gameRoutes)
-    }
-
-    start(): void {
-        this.app.listen(this.app.get('port'), () => {
-            console.log('Seridor corriendo en ', this.app.get('port'));
-        });
-    }
-
+  start(): void {
+    this.app.listen(this.app.get("port"), () => {
+      console.log("Seridor corriendo en ", this.app.get("port"));
+    });
+  }
 }
 
 const server = new Server();
